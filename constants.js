79f1d63a6b012551a6f5c13d67f1d7bb85b3fbfb
dima <mds@inbox.ru>
2014-09-22 00:24:00 +0600

    Correct constants

--- constants.js
+++ constants.js
@@ -20,9 +20,9 @@ Const.Timeouts = new Enum({
     INITIAL_CONFIRMATION_TIMEOUT:       5000,
     STANDARD_CONFIRMATION_TIMEOUT:      1000,
     INIT_STATE_MACHINE_STARTUP_DELAY:   1000,
-    ZIGBEE_RESPOND_TIMEOUT:             2000,
+    ZIGBEE_RESPOND_TIMEOUT:             10000,
     GROUP_STM_NEXT_DELAY:               100,
-    GROUP_STM_CICLE_WAIT:               5000
+    GROUP_STM_CICLE_WAIT:               2000
 });
 
 module.exports = Const;
