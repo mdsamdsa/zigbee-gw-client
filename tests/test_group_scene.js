0d5e063e5d7742e8bdd1994d3db32e0f15834dac
MyasnikovDS <mds@inbox.ru>
2014-09-29 12:21:31 +0600

    Test group and scene. Config for log4js

--- tests/test_group_scene.js
+++ tests/test_group_scene.js
@@ -3,6 +3,7 @@
 var module_name = module.filename.slice(module.filename.lastIndexOf(require('path').sep)+1, module.filename.length -3);
 
 var log4js = require('log4js');
+log4js.configure('../log4js.json', {});
 var logger = log4js.getLogger(module_name);
 var when = require('when');
 
@@ -32,8 +33,19 @@ Profiles.on('ready', function() {
     var group_stm = new GroupStm(proxy, pan, engines, main_stm);
     var scene_stm = new SceneStm(proxy, pan, engines, main_stm);
 
-    function sequence(arr, arg1, arg2) {
-
+    function sequence(arr, arg1, arg2, arg3) {
+        var deferred = when.defer();
+        function _sequence() {
+            if (arr.length != 0) {
+                var task = arr.shift();
+                var promise = task.call(this, arg1, arg2, arg3);
+                when(promise).finally(_sequence);
+            } else {
+                deferred.resolve(true);
+            }
+        }
+        _sequence();
+        return deferred.promise;
     }
 
     function test_group_scene() {
@@ -41,41 +53,73 @@ Profiles.on('ready', function() {
             logger.error('Device for test not found');
             return;
         }
+        //noinspection JSPotentiallyInvalidConstructorUsage
         var address = new Protocol.GatewayMgr.gwAddressStruct_t();
         address.addressType = Protocol.GatewayMgr.gwAddressType_t.UNICAST;
         address.ieeeAddr = pan.devices[1].ieeeAddress;
         address.endpointId = pan.devices[1].endpoints[0].endpointId;
         var groupId = 10;
+        var sceneId = 2;
         sequence([
             function(address, groupId) {
-                when(engines.group_scene.send_add_group_request(address, groupId, ''))
+                return when(engines.group_scene.send_add_group_request(address, groupId, ''))
                     .then(engines.group_scene.process_add_group_cnf)
-                    .then(function (msg) {
+                    .then(function(msg) {
                         return proxy.wait('GATEWAY', msg.sequenceNumber, Const.Timeouts.ZIGBEE_RESPOND_TIMEOUT.value)
                     }.bind(this))
                     .then(engines.group_scene.process_add_group_rsp_ind)
-                    .then(function (msg) {
+                    .then(function() {
                         logger.debug('group added succesfull');
                     })
-                    .catch(function (err) {
+                    .catch(function(err) {
                         logger.warn('group added failure: ' + err);
                     });
             },
+            function(address, groupId, sceneId) {
+                return when(engines.group_scene.send_store_scene_request(address, groupId, sceneId))
+                    .then(engines.group_scene.process_store_scene_cnf)
+                    .then(function(msg) {
+                        return proxy.wait('GATEWAY', msg.sequenceNumber, Const.Timeouts.ZIGBEE_RESPOND_TIMEOUT.value)
+                    }.bind(this))
+                    .then(engines.group_scene.process_store_scene_rsp_ind)
+                    .then(function() {
+                        logger.debug('scene stored succesfull');
+                    })
+                    .catch(function(err) {
+                        logger.warn('scene stored failure: ' + err);
+                    });
+            },
+            function(address, groupId, sceneId) {
+                return when(engines.group_scene.send_remove_scene_request(address, groupId, sceneId))
+                    .then(engines.group_scene.process_remove_scene_cnf)
+                    .then(function(msg) {
+                        return proxy.wait('GATEWAY', msg.sequenceNumber, Const.Timeouts.ZIGBEE_RESPOND_TIMEOUT.value)
+                    }.bind(this))
+                    .then(engines.group_scene.process_remove_scene_rsp_ind)
+                    .then(function() {
+                        logger.debug('scene removed succesfull');
+                    })
+                    .catch(function(err) {
+                        logger.warn('scene removed failure: ' + err);
+                    });
+            },
             function(address, groupId) {
-                when(engines.group_scene.send_remove_from_group_request(address, groupId))
+                return when(engines.group_scene.send_remove_from_group_request(address, groupId))
                     .then(engines.group_scene.process_remove_from_group_cnf)
-                    .then(function (msg) {
+                    .then(function(msg) {
                         return proxy.wait('GATEWAY', msg.sequenceNumber, Const.Timeouts.ZIGBEE_RESPOND_TIMEOUT.value)
                     }.bind(this))
                     .then(engines.group_scene.process_remove_from_group_rsp_ind)
-                    .then(function (msg) {
+                    .then(function() {
                         logger.debug('group removed succesfull');
                     })
-                    .catch(function (err) {
+                    .catch(function(err) {
                         logger.warn('group removed failure: ' + err);
                     });
             }
-        ], address, groupId);
+        ], address, groupId, sceneId).then(function() {
+            clearTimeout(timer);
+        });
     }
 
     var timer = setTimeout(function() {
