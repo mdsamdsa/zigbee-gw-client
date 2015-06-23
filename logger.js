fa449a13c145ef3bb0a5f689335ed0df3ea6e489
MyasnikovDS <mds@inbox.ru>
2015-06-23 20:53:53 +0500

    Refactor logging

--- logger.js
+++ logger.js
@@ -5,7 +5,7 @@ var log = require('log4js');
 var getLogger = log.getLogger;
 
 log.getLogger = function(category) {
-    return getLogger.call(log, category);
+    return getLogger.call(log, "zgwc:" + category);
 };
 
 module.exports = log;
