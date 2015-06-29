9897af0c4486b4d99ea739f408e20bd2271ecf86
MyasnikovDS <Dmitrij.Myasnikov@pnn.lukoil.com>
2015-06-29 17:47:07 +0500

    Test reset message

--- tests/test_client.js
+++ tests/test_client.js
@@ -1,12 +1,44 @@
 'use strict';
 
+var module_name = module.filename.slice(module.filename.lastIndexOf(require('path').sep)+1, module.filename.length -3);
+
 var Logger = require('../logger');
-Logger.configure('./log4js.json', {});
+//Logger.configure('./log4js.json', {});
+var logger = Logger.getLogger(module_name);
+
+var when = require('when');
 
 var client = require('../client');
+var Protocol = require('../protocol');
+
+var timeout = setTimeout(function() {
+    client.stop();
+}, 60 * 1000);
+
+var cnt = 0;
+
+client.attr_stm.on('done', function() {
+    if (cnt++ < 2) {
+        client.proxy._nwk_server.socket.destroy();
+        logger.info('reset init');
+        /*when(client.engines.nwk.local_device.send_zigbee_system_reset_request(Protocol.NWKMgr.nwkResetMode_t.SOFT_RESET))
+            .finally();
+        /*when(client.engines.nwk.local_device.send_zigbee_system_reset_request(Protocol.NWKMgr.nwkResetMode_t.SOFT_RESET))
+            .then(client.engines.nwk.local_device.process_zigbee_system_reset_cnf)
+            .then(function() {
+                logger.info('reset complete');
+            })
+            .catch(function (err) {
+                logger.error(err);
+            });
+            .finally(function () {
+                clearTimeout(timeout);
+                setTimeout(function() {
+                    client.stop();
+                }, 10 * 1000);
+            });*/
+    }
+});
+
 
 client.start();
-
-setTimeout(function() {
-    client.stop();
-}, 10 * 1000);
