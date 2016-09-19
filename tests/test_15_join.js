7110ffa18e5c846c312e020dc50d30459fd4aff0
MyasnikovDS <mds@inbox.ru>
2016-09-19 21:12:07 +0500

    Add test for permit join, leave and join devices

---
+++ tests/test_15_join.js
@@ -0,0 +1,34 @@
+'use strict';
+
+var module_name = module.filename.slice(module.filename.lastIndexOf(require('path').sep)+1, module.filename.length -3);
+
+var Logger = require('../logger');
+Logger.configure('./log4js.json', {});
+var logger = Logger.getLogger(module_name);
+
+var when = require('when');
+
+var Protocol = require('../protocol');
+var client = require('../client');
+
+var timeout = setTimeout(function() {
+    client.stop();
+}, 120 * 1000);
+
+var join = false;
+client.attr_stm.on('done', function () {
+    if (!join) {
+        join = true;
+        when(client.engines.nwk.network.send_set_permit_join_request(Protocol.NWKMgr.nwkPermitJoinType_t.PERMIT_NETWORK, 1020))
+            .then(client.engines.nwk.network.process_set_permit_join_cnf)
+            .tap(function (msg) {
+                if (msg.status == 0)
+                    logger.debug("Join permitted");
+            })
+            .catch(function (err) {
+                logger.error(err.message);
+            });
+    }
+});
+
+client.start();
